3dd79b45998a3c2a14015dad4393b374dc788111
sohyp <sohyp220@gmail.com>
2026-03-14 17:58:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const quran = {
   },
   mounted() {
     // جلب قائمة السور وتخزينها في surahs
-    fetch('http://api.alquran.cloud/v1/surah')
+    fetch('https://api.alquran.cloud/v1/surah')
       .then(res => res.json())
       .then(data => {
         this.surahs = data.data; // هنا التعديل المهم
@@ -49,7 +49,7 @@ const quran = {
   methods: {
     fetchSurah(id) {
       // جلب تفاصيل السورة
-      fetch(`http://api.alquran.cloud/v1/surah/${id}/quran-uthmani`)
+      fetch(`https://api.alquran.cloud/v1/surah/${id}/quran-uthmani`)
         .then(res => res.json())
         .then(json => {
           this.selectedSurah = json.data; // التعديل هنا (json.data مباشرة)
@@ -60,15 +60,103 @@ const quran = {
   }
 }
 
-// مكونات فرعية بسيطة
 const azkar = {
-  template: '<div class="page"><h1>الأذكار قريباً...</h1></div>'
-}
-const sabah = {
-  template: '<div class="page"><h1>السبحة الإلكترونية</h1></div>'
+  data() {
+    return {
+      azkarSabah: [],
+      azkarMasaa: [],
+      adeya : [],
+      adeyaT: [],
+      sleep: [],
+      tsabeeh: [],
+      azkarIst: [],
+      abimaa:[],
+      showContent: false,
+      type:''
+    }
+  },
+  mounted() {
+    fetch('https://raw.githubusercontent.com/nawafalqari/azkar-api/56df51279ab6eb86dc2f6202c7de26c8948331c1/azkar.json')
+      .then(res => res.json())
+      .then(json => {
+        const azkarSabah = json['أذكار الصباح']
+        this.azkarSabah = azkarSabah
+        const azkarMasaa= json['أذكار المساء']
+        this.azkarMasaa = azkarMasaa
+        const adeya = json['أدعية الأنبياء']
+        this.adeya = adeya
+        const adeyaT = json['أدعية قرآنية']
+        this.adeyaT = adeyaT
+        const sleep = json['أذكار النوم']
+        this.sleep = sleep
+        const tsabeeh = json['تسابيح']
+        this.tsabeeh = tsabeeh
+        const azkarIst = json['أذكار الاستيقاظ']
+        this.azkarIst = azkarIst
+        const abimaa = json['أذكار بعد السلام من الصلاة المفروضة']
+        this.abimaa = abimaa
+      })
+      .catch(err => console.error('Error:', err))
+  },
+  methods: {
+    content(type) {
+      this.showContent = true
+      this.type = type
+    },
+    Ncontent(){
+      this.showContent = false
+    }
+  },
+  template: `
+<button @click="content('sabah')"> أذكار الصباح </button>
+<button @click="content('tsabeeh')"> تسابيح </button>
+<button @click="content('azkarIst')"> أذكار الاستيقاظ من النوم </button>
+<button @click="content('abimaa')"> أذكار السلام من الصلاة  </button>
+<button @click="content('masaa')"> أذكار المساء </button>
+<button @click="content('adeya')"> ادعيه الانبياء </button>
+<button @click ="content('adeyaT')">ادعيه القران </button>
+<button @click="content('sleep')"> أذكار النوم </button>
+<button @click="Ncontent()">عودة للأذكار</button>
+<div v-if="showContent" style='margin-top: 300px;'>
+  <h1 v-for="azkar in (type === 'sabah' ? azkarSabah : type === 'masaa' ? azkarMasaa : type === 'adeya' ? adeya : type === 'adeyaT' ? adeyaT : type === 'sleep' ? sleep : type === 'tsabeeh' ? tsabeeh : type === 'azkarIst' ? azkarIst : type === 'abimaa' ? abimaa : [])" :key="azkar.content" style="font-size: 25px;">
+    {{ azkar.content }}
+  </h1>
+</div>
+
+  `
 }
 
-
+const sapah = {
+  data() {
+    return {
+      count: 0,
+      type: '',
+      types: ['تسبيح', 'حمد', 'تكبير', 'تهليل', 'استغفار']
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    zt(type) {
+      this.type = type
+      this.count = 0
+    },
+    increment() {
+      if (this.type) {
+        this.count++
+      }
+    }
+  },
+  template: `
+    <div>
+      <div class='count' style="margin: auto;">
+        <button @click="increment()">اذكر الله</button>
+      </div>
+      <h2 v-if="type" style="margin: auto;">{{ count }} :{{type}} الذكر</h2>
+      <button v-for="(t, index) in types" :key="index" @click="zt(t)" style="margin: 5px;">{{ t }}</button>
+    </div>
+  `
+}
 const home = {
   template: `
    <header>{ بســــم اللــه الرحمـــن الرحيـــم }
@@ -76,7 +164,7 @@ const home = {
        تطبيق القرآن والذكر 
   </header>
   
-  <div>: الصلاة القادمة
+  <div class='sa'>: الصلاة القادمة
   </div>
   <div class="cont">
     <img src="istekame.jpg" alt="">
@@ -96,7 +184,7 @@ const home = {
 const routes = [
   { path: '/quran', component: quran },
   { path: '/azkar', component: azkar },
-  { path: '/sabah', component: sabah },
+  { path: '/sabah', component: sapah },
   { path: '/', component: home }
 ]
 
